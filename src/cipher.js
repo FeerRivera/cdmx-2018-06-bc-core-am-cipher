0ee999efae2b0d0ee0149ffdfd366c30e663e4c6
FeerRivera <feerkay15@gmail.com>
2018-06-12 12:00:33 -0500

    cifrado y descifrado con la llave que ingresa el usuario

--- src/cipher.js
+++ src/cipher.js
@@ -2,45 +2,34 @@
 function cipher() {
     let phrase = document.getElementById("txt").value.toUpperCase();
     let key = parseInt(document.getElementById("salto").value);
-console.log(phrase,key);
-    //mensaje = document.getElementById('texto').value;
+//console.log(phrase,key);
     let string = [];
-    //phrase = phrase.replace(/\s/g, '').toLowerCase();
     for (let i = 0; i < phrase.length; i++) {
-        let character = String.fromCharCode((phrase.charCodeAt(i) -65+key)%26+65); //declaramos la Variable character que leer� la palabra y con String.fromCharCode nos devolver� la cadena mediante el uso de secuencia de valores//
-console.log(character);
-        string.push(character);
 
-        //let joinLetters = string.join("");
+        let character = String.fromCharCode((phrase.charCodeAt(i) -65+key)%26+65); //declaramos la Variable character que leer� la palabra y con String.fromCharCode nos devolver� la cadena mediante el uso de secuencia de valores//
+//console.log(character);
+        string.push(character);
     }
-    joinLetters = document.getElementById("txtCifrado").value;
+    let joinLetters = string.join("");
+    document.getElementById("txtCifrado").value = joinLetters;
     return joinLetters;
 }
 
 
-function decipher(phrase) {
-    let string = [];
+function decipher() {
+    let phrase2 = document.getElementById("txtCifrado").value.toUpperCase();
+    let key = parseInt(document.getElementById("salto").value);
 
-    //phrase = phrase.toLowerCase().replace(/\s/g, '');
-
-    for (let i = 0; i < phrase.length; i++) {
-        //let character2 = String.fromCharCode((phrase[i].charCodeAt() +65-key)%26+65);
-let character2 = String.fromCharCode((phrase.charCodeAt(i) +65-key)%26+65);
+   let string = [];
+    for (let i = 0; i < phrase2.length; i++) {
+        let character2 = String.fromCharCode((phrase2.charCodeAt(i) +65-key)%26+65); //declaramos la Variable character que leer� la palabra y con String.fromCharCode nos devolver� la cadena mediante el uso de secuencia de valores//
+//console.log(character2);
         string.push(character2);
-
-        //let joinLetters2 = string.join("");
-    }
-  joinLetters2= document.getElementById("txtDecifrado").value;
+}
+      let joinLetters2 = string.join("");
+    document.getElementById("txtDecifrado").value = joinLetters2;
     return joinLetters2;
 }
-
-
-//console.log(cipher(phrase));
-//console.log(decipher(cipher(phrase)));
-
-
-//document.getElementById("txtDecifrado").value = decipher(phrase);
-//console.log(decipher(cipher(phrase)));
 
 
 //console.log(decipher(cipher(phrase)));
